a180d4776af8a7ee51ad57b170a0e4aeedf9c4fb
SachaAttiach <settiach@hotmail.co.uk>
2022-04-10 13:45:50 +0300

    server is running

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Chat from "./components/Chat";
+import Join from "./components/Join";
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
