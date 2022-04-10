7ad8919637fe305c0ce2ab54c475cfb55fc73f3f
SachaAttiach <settiach@hotmail.co.uk>
2022-04-10 14:34:45 +0300

    implemented useEffect wth socket

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//to help get data from the url
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  //perhaps put these two into context so I don't repeat code.
+  //also, location.search is something we use from react router dom
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    console.log(socket);
+  }, [ENDPOINT, location.search]);
   return <h1>Chat</h1>;
 };
 export default Chat;
